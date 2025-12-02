da6209e5e53403337ddecd92411d6169bf8bee29
deci02 <d.cielaszyk@gmail.com>
2025-12-02 10:53:35 +0100

    fertig, inklusive gesamt und Formatierung

--- Schule/src/vererbung/Verein.java
+++ Schule/src/vererbung/Verein.java
@@ -1,5 +1,6 @@
 package vererbung;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class Verein {
@@ -11,11 +12,15 @@ public class Verein {
 	}
 
 	public void ausgebenGehaltsliste() {
+		double gesamt = 0;
 		System.out.println("Typ\tName\tVorname\tJahresgehalt\tSonstiges");
 		System.out.println("--------------------------------------");
 		for (int i = 0; i < this.mitglieder.size(); i++) {
 			System.out.println(this.mitglieder.get(i).erzeugenZeile());
+			gesamt += this.mitglieder.get(i).gehalt;
 		}
+		System.out.println(
+				"Summe der Jahresgehälter: ".concat(new DecimalFormat("#,##0.00").format(gesamt)).concat(" €"));
 	}
 
 }
